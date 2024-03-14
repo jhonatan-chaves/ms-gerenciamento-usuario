f55213e5d938201d05ed1a46e0778ec5ca110e5d
jhonatan-chaves <cjonata799@gmail.com>
2024-03-14 03:36:29 -0300

    iniciandio o micro-service de gerenciamento de usuario

---
+++ src/main/java/com/jhonatan/GerenciamentoUsuarioApplication.java
@@ -0,0 +1,14 @@
+package com.jhonatan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoUsuarioApplication.class, args);
+		System.out.println("estamos rodando!");
+	}
+
+}
---
+++ src/test/java/com/jhonatan/GerenciamentoUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhonatan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
